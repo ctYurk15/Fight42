a96a12ad4fc5fabfa4ebe61a9ee8005b5772b032
ctYurk15 <44519773+ctYurk15@users.noreply.github.com>
2019-01-21 18:13:48 +0200

    v 0.2
    New features:
    1)Changing game cycle.
    2)Replaced "Kick" to "Foot-Kick"
    3)New kick - "Hand-Kick". 10 HP damage

--- Fight42/Fight42/Program.cs
+++ Fight42/Fight42/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fight42
 {
@@ -15,41 +11,58 @@ namespace Fight42
         //main method
         static void Main(string[] args)
         {
-            string act , f= "First", s = "Second";
             Console.WriteLine("Welcome to Fight42! Lets Start!");
-            while(health[0] > 0 && health[1] > 0)
-            {
-                Console.WriteLine("First Player: ");
-                act = Convert.ToString(Console.ReadLine());
-                Behavior(ref f, act, ref health[0], ref health[1]);
-                Console.WriteLine("Second Player:");
-                act = Convert.ToString(Console.ReadLine());
-                Behavior(ref s, act, ref health[1], ref health[0]);
-            }
-            if (health[0] <= 0)
-                Console.WriteLine("Second Player Wins! PAKTE!");
-            else if (health[1] <= 0)
-                Console.WriteLine("First Player Wins! PAKTE! ");
+            GameCycle();
             Console.ReadKey();
         }
 
+        //game cycle, return result
+        static void GameCycle()
+        {
+            string act;
+            if (health[0] > 0)
+            {
+                Console.WriteLine("First Player: ");
+                act = Convert.ToString(Console.ReadLine());
+                Behavior("First", act, ref health[0], ref health[1]);
+            }
+            else
+            {
+                Console.WriteLine("Second Player win! P.A.K.T.E.>!!!");
+            }
+            if (health[1] > 0)
+            {
+                Console.WriteLine("Second Player:");
+                act = Convert.ToString(Console.ReadLine());
+                Behavior("Second", act, ref health[1], ref health[0]);
+                GameCycle();
+            }
+            else
+                Console.WriteLine("First Player win! P.A.K.T.E.!!!");
+        }
+
         //Actions
-        static void Behavior(ref string player, string do_act, ref int PH, ref int EH)
+        static void Behavior(string player, string do_act, ref int PH, ref int EH)
         {
             switch(do_act)
             {
                 case "Health":
-                    PH = PH + 10;
-                    Console.WriteLine($"{player} player healed self");
+                    PH = PH + 12;
+                    Console.WriteLine($"{player} player healed self!");
                     Console.WriteLine($"First: {health[0]}hp vs Second: {health[1]}hp");
                     break;
-                case "Kick":
-                    EH = EH - 20;
-                    Console.WriteLine($"{player} player kicked another");
+                case "Foot-Kick":
+                    EH = EH - 15;
+                    Console.WriteLine($"{player} player kicked with foot another!");
+                    Console.WriteLine($"First: {health[0]}hp vs Second: {health[1]}hp");
+                    break;
+                case "Hand-Kick":
+                    EH = EH - 10;
+                    Console.WriteLine($"{player} player kicked with hand another!");
                     Console.WriteLine($"First: {health[0]}hp vs Second: {health[1]}hp");
                     break;
                 default:
-                    Console.WriteLine($"{player} player made nothing");
+                    Console.WriteLine($"{player} player made nothing!");
                     Console.WriteLine($"First: {health[0]}hp vs Second: {health[1]}hp");
                     break;
             }
