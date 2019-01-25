5f2efb498012d5ca38ae7a8f5f2eb9ed9b1da2f7
ctYurk15 <44519773+ctYurk15@users.noreply.github.com>
2019-01-25 13:56:24 +0200

    v 0.4
    Features:
    1)Now max HP = 100hp, no more greater;
    2)Added fatality`s;

--- Fight42/Fight42/Program.cs
+++ Fight42/Fight42/Program.cs
@@ -11,7 +11,7 @@ namespace Fight42
         //main method
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Fight42! Lets Start!");
+            Console.WriteLine("|Welcome to Fight42! Lets Start!|");
             GameCycle();
             Console.ReadKey();
         }
@@ -22,24 +22,24 @@ namespace Fight42
             string action = "Empty";
             if(health[0] > 0)
             {
-                Console.Write("|First Player  |");
+                Console.Write("|First Player  |: ");
                 action = Console.ReadLine();
                 Behavior("First", action, ref health[0], ref health[1]);
                 if(health[1] > 0)
                 {
-                    Console.Write("|Second Player |");
+                    Console.Write("|Second Player |: ");
                     action = Console.ReadLine();
                     Behavior("Second", action, ref health[1], ref health[0]);
                     GameCycle();
                 }
                 else
                 {
-                    Console.WriteLine("Second Player wins!");
+                    Console.WriteLine("|Second Player wins!|");
                 }
             }
             else
             {
-                Console.WriteLine("First Player wins!");
+                Console.WriteLine("|First Player wins!|");
             }
 
         }
@@ -49,35 +49,71 @@ namespace Fight42
         {
             switch(do_act)
             {
+                //healthing
                 case "Health":
-                    PH = PH + 12;
-                    Console.WriteLine($"{player} player healed self!");
+                    //-----------//  dont let up HP higher than 100                       
+                    if (PH < 89)                          
+                    {                                       
+                        PH += 12;                          
+                        Console.WriteLine("|+12 HP|");    
+                    }
+                    else
+                    {
+                        Console.WriteLine($"|+{100 - PH}|");
+                        PH = 100;
+                    }
+                    //---------//
+                    Console.WriteLine($"|{player} player healed self!|");
                     Console.WriteLine($"First: {health[0]}hp vs Second: {health[1]}hp");
                     break;
+                //kicking
                 case "Kick":
+                    if(EH <= 15)
+                    {
+                        Console.WriteLine("|Footballer fatality!|");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"|-15 HP|\n|{player} player kicked with foot another!|");
+                    }
                     EH = EH - 15;
-                    Console.WriteLine($"{player} player kicked with foot another!");
-                    Console.WriteLine($"First: {health[0]}hp vs Second: {health[1]}hp");
+                    Console.WriteLine($"|First: {health[0]}hp vs Second: {health[1]}hp|");
                     break;
+                //hit with hand
                 case "Hand-Hit":
+                    if (EH <= 10)
+                        Console.WriteLine("Boxer Fatality!");
+                    else
+                        Console.WriteLine($"|-10 HP|\n|{player} player kicked with hand another!|");
                     EH = EH - 10;
-                    Console.WriteLine($"{player} player kicked with hand another!");
-                    Console.WriteLine($"First: {health[0]}hp vs Second: {health[1]}hp");
+                    Console.WriteLine($"|First: {health[0]}hp vs Second: {health[1]}hp|");
                     break;
+                //vampirizm
                 case "Vampirizm":
                     if(EH >= 9)
                     {
                         EH = EH - 9;
-                        PH = PH + 9;
-                        Console.WriteLine($"{player} taked some HP from another");
-                        Console.WriteLine($"First: {health[0]}hp vs Second: {health[1]}hp");
+                        if(PH < 92)
+                        {
+                            PH = PH + 9;
+                            Console.WriteLine("|+9 HP|");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"|+{100 - PH}|");
+                            PH = 100;
+                        }
+                        Console.WriteLine("|-9 HP|");
+                        Console.WriteLine($"|{player} taked some HP from another|");
+                        Console.WriteLine($"|First: {health[0]}hp vs Second: {health[1]}hp|");
                     }
                     else
                     {
                         EH = 0;
-                        Console.WriteLine("Another player was too weack and dead in proccess.");
+                        Console.WriteLine("|Another player was too weak and dead in proccess.|\n|Vampir Fatality!|");
                     }
                     break;
+                //nothing
                 default:
                     Console.WriteLine($"{player} player made nothing!");
                     Console.WriteLine($"First: {health[0]}hp vs Second: {health[1]}hp");
