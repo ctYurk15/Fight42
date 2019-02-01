bfd04a68f24976fa5e65ac9cf24c536ee07c6841
ctYurk15 <44519773+ctYurk15@users.noreply.github.com>
2019-02-01 12:31:26 +0200

    v 0.6
    Features:
    1)Fixed bugs;
    2)now force of hits player is bigging:
    We have first "norma" - 2. When one of player have much kicks that "norma", to his force multiplier add 0.5 and norma = norma * 2;

--- Fight42/Fight42/Program.cs
+++ Fight42/Fight42/Program.cs
@@ -4,20 +4,53 @@ namespace Fight42
 {
     class Program
     {
-        //data of players(hp, kicks, all damage)
-        public static int[] data1 = { 40, 0, 0 };
-        public static int[] data2 = { 40, 0, 0 };
+        //data of players(hp, kicks, all damage, coeficient of damage)
+        public static double[] data1 = { 100.0, 0.0, 0.0, 1.0 };
+        public static double[] data2 = { 100.0, 0.0, 0.0, 1.0 };
+        //just norma
+        public static int norma = 2;
+
+        //make power of players bigger
+        static void ivents()
+        {
+            if(data1[1] > norma)
+            {
+                data1[3] += 0.5;
+                norma = norma * 2;
+                Console.WriteLine("Force of First player is bigger!");
+            }
+            else if(data2[1] > norma)
+            {
+                data1[3] += 0.5;
+                norma = norma * 2;
+                Console.WriteLine("Force of Second player  is bigger!");
+            }
+        }
+
+        static void lOGO()
+        {
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("-|||||--|-|||||||-|---|-|||||||---|-----|-|||||||");
+            Console.WriteLine("-|------|-|-------|---|----|------|-----|-------|");
+            Console.WriteLine("-|||||--|-|-------|||||----|------|||||||-------|");
+            Console.WriteLine("-|------|-|---||--|---|----|------------|-|||||||");
+            Console.WriteLine("-|------|-|-----|-|---|----|------------|-|------");
+            Console.WriteLine("-|------|-|||||||-|---|----|------------|-|||||||");
+            Console.WriteLine("-------------------------------------------------");
+        }
 
 
         //main method
         static void Main(string[] args)
         {
+            lOGO();
             Console.WriteLine("|Welcome to Fight42! Lets Start!|");
             GameCycle();
             Console.WriteLine("First player:");
             Console.WriteLine($"Total kicks: {data1[1]} Total damage: {data1[2]}");
             Console.WriteLine("Second player:");
             Console.WriteLine($"Total kicks: {data2[1]} Total damage: {data2[2]}");
+            lOGO();
             Console.ReadKey();
         }
 
@@ -27,11 +60,13 @@ namespace Fight42
             string action = "Empty";
             if(data1[0] > 0)
             {
+                ivents();
                 Console.Write("|First Player  |: ");
                 action = Console.ReadLine();
                 Behavior("First", action, ref data1, ref data2);
                 if(data2[0] > 0)
                 {
+                    ivents();
                     Console.Write("|Second Player |: ");
                     action = Console.ReadLine();
                     Behavior("Second", action, ref data2, ref data1);
@@ -39,18 +74,18 @@ namespace Fight42
                 }
                 else
                 {
-                    Console.WriteLine("|Second Player wins!|");
+                    Console.WriteLine("|First Player wins!|");
                 }
             }
             else
             {
-                Console.WriteLine("|First Player wins!|");
+                Console.WriteLine("|Second Player wins!|");
             }
 
         }
 
         //Actions
-        static void Behavior(string player, string do_act, ref int[] PD, ref int[] ED)
+        static void Behavior(string player, string do_act, ref double[] PD, ref double[] ED)
         {
             switch(do_act)
             {
@@ -74,42 +109,44 @@ namespace Fight42
                 //kicking
                 case "Kick":
                     PD[1] += 1;
-                    if (ED[0] <= 15)
+                    if (ED[0] <= 15 * PD[3])
                     {
-                        PD[2] = ED[0] - 15;
+                        PD[2] += ED[0];
+                        ED[0] = 0;
                         Console.WriteLine("|Footballer fatality!|");
                     }
                     else
                     {
-                        PD[2] += 15;
-                        Console.WriteLine($"|-15 HP|\n|{player} player kicked with foot another!|");
+                        PD[2] += 15 * PD[3];
+                        ED[0] = ED[0] - 15 * PD[3];
+                        Console.WriteLine($"|-{15 * PD[3]} HP|\n|{player} player kicked with foot another!|");
+                        Console.WriteLine($"|First: {data1[0]}hp vs Second: {data2[0]}hp|");
                     }
-                    ED[0] = ED[0] - 15;
-                    Console.WriteLine($"|First: {data1[0]}hp vs Second: {data2[0]}hp|");
                     break;
                 //hit with hand
                 case "Hand-Hit":
                     PD[1] += 1;
-                    if (ED[0] <= 10)
+                    if (ED[0] <= 10 * PD[3])
                     {
-                        PD[2] += ED[0] - 10;
+                        PD[2] += ED[0];
+                        ED[0] = 0;
                         Console.WriteLine("Boxer Fatality!");
                     }
                     else
                     {
-                        PD[2] += 10;
-                        Console.WriteLine($"|-10 HP|\n|{player} player kicked with hand another!|");
+                        PD[2] += 10 * PD[3];
+                        Console.WriteLine($"|-{10 * PD[3]} HP|\n|{player} player kicked with hand another!|");
+                        ED[0] = ED[0] - 10 * PD[3];
+                        Console.WriteLine($"|First: {data1[0]}hp vs Second: {data2[0]}hp|");
                     }
-                    ED[0] = ED[0] - 10;
-                    Console.WriteLine($"|First: {data1[0]}hp vs Second: {data2[0]}hp|");
                     break;
                 //vampirizm
                 case "Vampirizm":
                     PD[1] += 1;
-                    if(ED[0] >= 9)
+                    if(ED[0] > 11)
                     {
-                        ED[0] = ED[0] - 9;
-                        PD[2] += 9;
+                        ED[0] = ED[0] - 11;
+                        PD[2] += 11;
                         if(PD[0] < 92)
                         {
                             PD[0] = PD[0] + 9;
@@ -120,7 +157,7 @@ namespace Fight42
                             Console.WriteLine($"|+{100 - PD[0]}|");
                             PD[0] = 100;
                         }
-                        Console.WriteLine("|-9 HP|");
+                        Console.WriteLine("|-11 HP|");
                         Console.WriteLine($"|{player} taked some HP from another|");
                         Console.WriteLine($"|First: {data1[0]}hp vs Second: {data2[0]}hp|");
                     }
